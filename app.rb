b66daea3131e6cfbb5b1aed94e786d2f61671578
Alexandra Palermo <alexmpalermo@gmail.com>
2019-03-27 17:29:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,6 +14,6 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @num = params[:number]
     @phrase = params[:phrase]
-    @phrase.times do 
+    @num.to_i.times do |@phrase|
 
 end
