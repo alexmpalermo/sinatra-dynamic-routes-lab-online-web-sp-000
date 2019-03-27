9cd940d7b1ceb166a05a19e9299e282cacdd2a88
Alexandra Palermo <alexmpalermo@gmail.com>
2019-03-27 17:29:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,17 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   get '/reversename/:name' do 
     @user_name = params[:name]
-    @user_name.reverse 
+    "#{@user_name.reverse}"
   end
+
+  get '/square/:number' do
+    @num = params[:number]
+    "#{@num.to_i*@num.to_i}"
+  end
+
+  get '/say/:number/:phrase' do
+    @num = params[:number]
+    @phrase = params[:phrase]
+    @phrase.times do 
 
 end
